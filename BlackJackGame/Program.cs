7fba88ff1dfce226436a7e8f44ba7bc96ec29aa4
Dave <davida8861@gmail.com>
2023-01-28 07:24:25 -0500

    Fixed second player choice 'Hit ' conditional. Number generators go up to 13.

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -7,24 +7,24 @@ namespace BlackJackGame
         private static void Main(string[] args)
         {
             // Create random playing cards for player and computer
-            Random hitcard1 = new Random(); int hitcardone = hitcard1.Next(4, 12);
-            Random hitcard2 = new Random(); int hitcardtwo = hitcard2.Next(1, 8);
-            Random hitcard3 = new Random(); int hitcardthree = hitcard3.Next(1, 10);
-            Random showncard2 = new Random(); int showncardsecond = showncard2.Next(1, 10);
-            Random staycard = new Random(); int staycard1 = staycard.Next(1, 12);
-            Random staycard2 = new Random(); int staycardtwo = staycard2.Next(1, 12);
+            Random hitcard1 = new Random(); int hitcardone = hitcard1.Next(4, 13);
+            Random hitcard2 = new Random(); int hitcardtwo = hitcard2.Next(2, 13);
+            Random hitcard3 = new Random(); int hitcardthree = hitcard3.Next(2, 13);
+            Random showncard2 = new Random(); int showncardsecond = showncard2.Next(2, 13);
+            Random staycard = new Random(); int staycard1 = staycard.Next(2, 13);
+            Random staycard2 = new Random(); int staycardtwo = staycard2.Next(2, 13);
             string user_name, user_choice, user_choice2;
 
             // Ask for player name, initialize game, ask for player choice
             Console.WriteLine("Welcome to BlackJack! Before we start playing, what's your name?"); user_name = Console.ReadLine();
             Console.WriteLine("The rules are simple, " + user_name + ". The goal is to get as close to 21 without busting.");
             Random showncard = new Random();
-            int showncard1 = showncard.Next(3, 7);
+            int showncard1 = showncard.Next(3, 13);
             Console.WriteLine("I'm holding " + showncard1 + ". My other card isn't revealed.");
             Random number = new Random();
-            int num = number.Next(1, 8);
+            int num = number.Next(2, 13);
             Random number2 = new Random();
-            int num2 = number2.Next(1, 9);
+            int num2 = number2.Next(2, 13);
             Console.WriteLine("Your hand is " + num + " and " + num2 + ".");
             Console.WriteLine("Would you like to Hit or Stay?");
             user_choice = Console.ReadLine();
@@ -38,7 +38,7 @@ namespace BlackJackGame
                 if (user_choice2 == "Hit")
                 {
                     Console.WriteLine("Your next card is " + hitcardtwo + ". And your total is " + (num + num2 + hitcardone + hitcardtwo) + ".");
-                    if ((num + num2 + hitcardone + hitcardtwo) >= 21)
+                    if (num + num2 + hitcardone + hitcardtwo >= 21)
                     {
                         Console.WriteLine("Bust!");
                         return;
